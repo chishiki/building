e44dae2aeb2d0a448035687ba700d18487174a0c
Christopher Webb <chishiki@gmail.com>
2021-12-05 22:31:04 +0900

    resident schema

--- data/building-schema.sql
+++ data/building-schema.sql
@@ -59,3 +59,23 @@ CREATE TABLE `building_ReservableFacility` (
     `facilityEnabled` int(1) NOT NULL,
     PRIMARY KEY (`facilityID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+DROP TABLE IF EXISTS `building_Resident`;
+
+CREATE TABLE `building_Resident` (
+    `residentID` int(12) NOT NULL AUTO_INCREMENT,
+    `unitID` int(12) NULL,
+    `siteID` int(12) NOT NULL,
+    `creator` int(12) NOT NULL,
+    `created` datetime NOT NULL,
+    `updated` datetime NULL,
+    `deleted` int(1) NOT NULL,
+    `residentName` varchar(255) NOT NULL,
+    `residentStartDate` date NULL,
+    `residentEndDate` date NULL,
+    `residentStatus` varchar(20) NOT NULL,
+    `residentEnabled` int(1) NOT NULL,
+    PRIMARY KEY (`residentID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
